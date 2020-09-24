c323c5542b2e3b74113b2c061782177df3407419
Talia Garcia <eng.taliagg@gmail.com>
2020-09-24 18:30:08 -0500

    Redirect login-> home

--- Portal/ClientApp/src/app/components/login/login.component.ts
+++ Portal/ClientApp/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  name = new FormControl('');
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.router.navigate(['/home']);
   }
 
 }
