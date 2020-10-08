fdc2a01b311311fbae530ede2feef3b242b2f45d
Carlos Iván Montes Agüero <ci.montes@hotmail.com>
2020-10-07 22:22:12 -0500

    backend url change

--- Portal/ClientApp/src/environments/environment.prod.ts
+++ Portal/ClientApp/src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   urlData: null,
   token: "",
-  serviceEndpoint: "https://localhost:44364/api/",
+  //serviceEndpoint: "https://localhost:44364/api/",
+  serviceEndpoint: "http://74.208.178.79:4001/api/",
   localServices: {
     auth: "Auth",
   }
--- Portal/ClientApp/src/environments/environment.ts
+++ Portal/ClientApp/src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   urlData: null,
   token: "",
-  serviceEndpoint: "https://localhost:44364/api/",
+  //serviceEndpoint: "https://localhost:44364/api/",
+  serviceEndpoint: "http://74.208.178.79:4001/api/",
   localServices: {
     auth: "Auth",
   }
